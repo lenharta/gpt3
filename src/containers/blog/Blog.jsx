be635d3062384f9e481c4b6f0c7dd670a97ba135
Andrew Lenhart <andrew.code21@gmail.com>
2021-11-20 21:44:09 -0500

    Build Blog + Article + add imports

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { Article } from '../../components'
+import { blog01, blog02, blog03, blog04, blog05 } from './imports'
 import './blog.css'
 
 const Blog = () => {
   return (
-    <div>
-      Blog
+    <div className="gpt3__blog section__padding" id="blog">
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text">A lot is happening. We are blogging about it.</h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container_groupA">
+          <Article imgUrl={blog01} date="11/20/21" title="GPT-3 and OpenAI is the future. Let us explore it's impact" />
+        </div>
+        <div className="gpt3__blog-container_groupB">
+          <Article imgUrl={blog02} date="11/20/21" title="GPT-3 and OpenAI is the future. Let us explore it's impact" />
+          <Article imgUrl={blog03} date="11/20/21" title="GPT-3 and OpenAI is the future. Let us explore it's impact" />
+          <Article imgUrl={blog04} date="11/20/21" title="GPT-3 and OpenAI is the future. Let us explore it's impact" />
+          <Article imgUrl={blog05} date="11/20/21" title="GPT-3 and OpenAI is the future. Let us explore it's impact" />
+        </div>
+      </div>
     </div>
   )
 }
